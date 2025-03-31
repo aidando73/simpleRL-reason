bd381db72fdf69d01ecc74aeb556a40b7a2379bd
Aidan Do <aidando73@gmail.com>
2025-03-31 00:52:56 +0000

    .

--- train_grpo_math_tune_ray.sh
+++ train_grpo_math_tune_ray.sh
@@ -5,9 +5,6 @@ set -x
 export NCCL_DEBUG=INFO
 export RAY_BACKEND_LOG_LEVEL=debug
 export RAY_DEDUP_LOGS=0
-export CUDA_LAUNCH_BLOCKING=1
-export NCCL_P2P_DISABLE=1
-export NCCL_IB_DISABLE=1
 export VLLM_ATTENTION_BACKEND=XFORMERS
 # export NCCL_SOCKET_IFNAME=podnet1
 # export GLOO_SOCKET_IFNAME=podnet1
@@ -185,9 +182,6 @@ ray job submit --address=$MASTER_NODE_IP:6379 \
           "https_proxy": "",
           "NCCL_DEBUG": "'$NCCL_DEBUG'",
           "RAY_DEDUP_LOGS": "'$RAY_DEDUP_LOGS'",
-          "CUDA_LAUNCH_BLOCKING": "'$CUDA_LAUNCH_BLOCKING'",
-          "NCCL_P2P_DISABLE": "'$NCCL_P2P_DISABLE'",
-          "NCCL_IB_DISABLE": "'$NCCL_IB_DISABLE'",
           "VLLM_ATTENTION_BACKEND": "'$VLLM_ATTENTION_BACKEND'"
         }
     }' \
