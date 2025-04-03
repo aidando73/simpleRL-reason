0adafeb98dce97a2e077a784ec695ddbf731c933
Aidan Do <aidando73@gmail.com>
2025-04-04 09:38:05 +1100

    .

---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+# Use the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Security group for an EFA cluster
+resource "aws_security_group" "efa_cluster_sg" {
+  name = "efa-cluster-sg"
+  description = "Security group for an EFA cluster"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  # ssh
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+
+  # efa
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  tags = {
+    Name = "efa-cluster-sg"
+  }
+}
+
+
+
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-830c94e3"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
