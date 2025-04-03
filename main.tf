edbfc2f852b137f4bbc4909e4f58652bc2d5070d
Aidan Do <aidando73@gmail.com>
2025-04-04 09:47:25 +1100

    .

--- main.tf
+++ main.tf
@@ -39,17 +39,12 @@ resource "aws_security_group" "efa_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-  }
-
   # efa
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
     self = true
   }
 
