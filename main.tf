363c87022d3d659ae4e22b7763b55ee98b3d94a5
Aidan Do <aidando73@gmail.com>
2025-04-04 15:11:56 +1100

    .

--- main.tf
+++ main.tf
@@ -24,16 +24,16 @@ module "ebs_volume_west_2" {
   availability_zone = "us-west-2b"
 }
 
-module "cluster_west_2" {
-  source = "./modules/cluster"
-  providers = {
-    aws = aws.west2
-  }
-  ami_id = module.ami_west_2.ami_id
-  depends_on = [module.ami_west_2]  # Make sure AMI is ready
-  master_volume_id = module.ebs_volume_west_2.master_volume_id
-  worker_volume_id = module.ebs_volume_west_2.worker_volume_id
-  availability_zone = "us-west-2b"
-  instance_type = "gr6.8xlarge"
-#   capacity_block_id = ""
-}
+# module "cluster_west_2" {
+#   source = "./modules/cluster"
+#   providers = {
+#     aws = aws.west2
+#   }
+#   ami_id = module.ami_west_2.ami_id
+#   depends_on = [module.ami_west_2]  # Make sure AMI is ready
+#   master_volume_id = module.ebs_volume_west_2.master_volume_id
+#   worker_volume_id = module.ebs_volume_west_2.worker_volume_id
+#   availability_zone = "us-west-2b"
+#   instance_type = "gr6.8xlarge"
+# #   capacity_block_id = ""
+# }
